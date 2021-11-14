0c1ff935d8fed1160a5c1fef738932813431f864
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2021-11-14 14:15:34 -0600

    Birth

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,16 @@
 import pandas
 pandas.set_option('display.max_rows', None)
 data = pandas.read_csv("data.csv", low_memory=False)
-print(list(data))
-gglclck = data[data["type"].str.contains("Google Click")]
-print(gglclck)
+#print(list(data))
+#gglclck = data[data["type"].str.contains("Google Click")]
+#print(gglclck)
+clicktype = ["Dunkin Click", "Skyward Click"]
+#uatype = ["NaN"]
+dedupe = data.drop_duplicates(subset = ['email', 'ip'], keep=False)
+declicked = dedupe[dedupe.type.isin(clicktype)]
+#final = declicked[~declicked.useragent.isin(uatype)]
+#final = declicked[declicked.useragent.str.startswith("Mozilla")]
+final = declicked[~declicked['useragent'].isnull()]
+
+print(final)
+#print(list(data))
