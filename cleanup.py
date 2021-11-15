1565c2c8050cec3411e58cdf8dc38e0430095284
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2021-11-15 12:18:46 -0600

    HAHA NUMBERS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- cleanup.py
+++ cleanup.py
@@ -15,4 +15,46 @@ declicked = dedupe[dedupe.type.isin(clicktype)]
 final = declicked[~declicked['useragent'].isnull()]
 
 print(final)
+print()
 #final.to_csv("out.csv")
+
+totalpeople = 1743
+
+finalskyward = final[final.type.str.startswith("Skyward")]
+print("Amount of unique Skyward IP's: {0}".format(len(finalskyward)))
+mobiledevices = ["iPhone", "Android"]
+skywardsafari = finalskyward[finalskyward.useragent.str.contains("iPhone")]
+print("Amount of safari skyward clicks: {0}".format(len(skywardsafari)))
+skywardandroid = finalskyward[finalskyward.useragent.str.contains("Android")]
+print("Amount of Android skyward clicks: {0}".format(len(skywardandroid)))
+print("Amount of Mobile Skyward clicks: {0}".format(len(skywardandroid + skywardsafari)))
+print()
+skywardwindows = finalskyward[finalskyward.useragent.str.contains("Windows")]
+print("Amount of Windows clicks: {0}".format(len(skywardwindows)))
+skywardlinux = finalskyward[finalskyward.useragent.str.contains("Linux")]
+print("Amount of Linux clicks: {0}".format(len(skywardlinux)))
+print("Amount of Desktop and Laptop clicks: {0}".format(len(skywardwindows + skywardlinux)))
+print()
+
+clickrateperskyward = len(finalskyward) / totalpeople
+print(clickrateperskyward * 100)
+print()
+
+finaldunkin = final[final.type.str.startswith("Dunkin")]
+print("Amount of unique Dunkin IP's: {0}".format(len(finaldunkin)))
+mobiledevices = ["iPhone", "Android"]
+dunkinsafari = finaldunkin[finaldunkin.useragent.str.contains("iPhone")]
+print("Amount of safari Dunkin clicks: {0}".format(len(dunkinsafari)))
+dunkinandroid = finaldunkin[finaldunkin.useragent.str.contains("Android")]
+print("Amount of Android Dunkin clicks: {0}".format(len(dunkinandroid)))
+print("Amount of Mobile Dunkin clicks: {0}".format(len(dunkinandroid + dunkinsafari)))
+print()
+dunkinwindows = finaldunkin[finaldunkin.useragent.str.contains("Windows")]
+print("Amount of Windows clicks: {0}".format(len(dunkinwindows)))
+dunkinlinux = finaldunkin[finaldunkin.useragent.str.contains("Linux")]
+print("Amount of Linux clicks: {0}".format(len(dunkinlinux)))
+print("Amount of Desktop and Laptop clicks: {0}".format(len(dunkinwindows + dunkinlinux)))
+print()
+
+clickrateperdunkin = len(finaldunkin) / totalpeople
+print(clickrateperdunkin * 100)
